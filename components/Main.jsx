b4c5f2c0846ddf73b0ac89b3f4a326b75bb3479e
Mário Viegas <marioviegas2001@gmail.com>
2024-01-16 18:21:06 +0000

    Initial navbar + Main component

---
+++ components/Main.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Main() {
+  return (
+    <div className='hero-container'>
+        <div className="hero">
+            Mario Viegas
+        </div>
+    </div>
+  )
+}
+
+export default Main
---
+++ components/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function Navbar() {
+    return (
+      <div className="navbar">
+        <div className="left">
+          <span>Mário Viegas</span>
+          
+        </div>
+        <div className="center">
+          <span>About</span>
+          <span>Work</span>
+          <span>Socials</span>
+        </div>
+        <div className="right">
+          <span>Contact</span>
+        </div>
+      </div>
+    );
+  }
+
+export default Navbar;
