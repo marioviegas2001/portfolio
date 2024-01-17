e9835cda219790f3090a53d4adb7e47847b2e970
Mário Viegas <marioviegas2001@gmail.com>
2024-01-17 23:33:41 +0000

    Quote section added

---
+++ components/Quote.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Quote() {
+  return (
+    <div className="quote-container"> 
+        <div className='inspo'>
+            I'm a creator of  digital experiences,  one line of code at a time I'm dedicated to bringing ideas to life. 
+        </div>
+        <div className='quote'>
+            ❝The best way to predict the future is to invent it.❞ - Alan Kay
+        </div>  
+    </div>
+  )
+}
+
+export default Quote
