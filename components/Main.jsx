a96f374fe6df9b6d0a9c0dae25c04df80cfcaecc
Mário Viegas <marioviegas2001@gmail.com>
2024-01-17 19:55:54 +0000

    Main landing page base design

--- components/Main.jsx
+++ components/Main.jsx
@@ -1,11 +1,27 @@
+import Image from 'next/image'
 import React from 'react'
+import WebsiteHero from '../public/assets/WebsiteHero.png'
 
 function Main() {
   return (
     <div className='hero-container'>
-        <div className="hero">
-            Mario Viegas
+        <div className="hero1">
+            It's me, Mario
         </div>
+        <div className="hero2">
+            It's me, Mario
+        </div>
+        <div className="hero3">
+            It's me, Mario
+        </div>
+        <Image className='hero-image'
+            src={WebsiteHero} 
+            alt="Website Hero Pic"  
+            style={{
+              width: '50%',
+              height: 'auto',
+            }}
+        /> 
     </div>
   )
 }
