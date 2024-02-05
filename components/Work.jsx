16955532c3091b4eac24f0156681f0c7adacf30b
Mário Viegas <marioviegas2001@gmail.com>
2024-02-05 01:18:09 +0000

    work animations added

--- components/Work.jsx
+++ components/Work.jsx
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import gsap from "gsap/dist/gsap";
+import ScrollTrigger from "gsap/dist/ScrollTrigger";
+import SplitType from 'split-type';
 import Image from 'next/image';
-import sectionLogo from '../public/assets/sectionLogo.png';
 import placeholder from '../public/assets/placeholder.png';
 
 function WorkEntry({ name, imageSrc, reverse }) {
@@ -15,12 +17,42 @@ function WorkEntry({ name, imageSrc, reverse }) {
 }
 
 function Work() {
+  useEffect(() => {
+    // Check if window object is available (client-side)
+    if (typeof window !== 'undefined') {
+      gsap.registerPlugin(ScrollTrigger);
+      const work = new SplitType('.work-container span', { types: 'chars' });
+      const workChars = work.chars;
+
+      var tl = gsap.timeline();
+
+  
+      tl.from(workChars, {
+        scrollTrigger: {
+          trigger: '.work-container',
+          start: 'top center',
+          end: 'bottom center',
+          scrub: true,
+          markers: false
+        },
+        opacity: 0,
+        y: 50
+      }).to(".work-pic-container", {
+        scrollTrigger: {
+          trigger: '.work-pic-container',
+          start: 'top center',
+          end: 'center center',
+          scrub: true,
+          markers: true
+        },
+        opacity: 1,
+      });
+    }
+  }, []);
   return (
     <div id='work' className='work'>
       <div className='work-container'>
-        <Image className='section-logo' src={sectionLogo} alt='Section logo' />
         <span>WORK</span>
-        <Image className='section-logo' src={sectionLogo} alt='Section logo' />
       </div>
       <WorkEntry name="WORK 1" imageSrc={placeholder} reverse={false} />
       <WorkEntry name="WORK 2" imageSrc={placeholder} reverse={true} />
