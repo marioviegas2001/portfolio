32bf972b338a91bcea106f61748a914092640058
Mário Viegas <marioviegas2001@gmail.com>
2024-02-14 00:11:40 +0000

    Update Quote.jsx

--- components/Quote.jsx
+++ components/Quote.jsx
@@ -25,16 +25,29 @@ function Quote() {
         },
         opacity: 0.2,
         stagger: 0.1
-      }).from(charsQuote, {
-        scrollTrigger: {
-          trigger: '.quote',
-          start: 'top center',
-          end: "bottom center",
-          scrub: true,
-          markers: false
-        },
-        opacity: 0,
       });
+       // Create a new timeline for the charsQuote animation
+    var tlChars = gsap.timeline({ paused: true }); // Set paused to true to prevent auto-play
+
+    tlChars.from(charsQuote, {
+      opacity: 0,
+      stagger: 0.02,
+      onComplete: () => {
+        // Callback to ensure it only plays once
+        tlChars.kill(); // Kill the timeline after completion
+      }
+    });
+
+    // Add a scroll trigger to play the tlChars animation when the quote section is in view
+    ScrollTrigger.create({
+      trigger: '.quote',
+      start: 'top center',
+      end: 'bottom center',
+      onEnter: () => {
+        tlChars.play(); // Play the tlChars animation when the quote section is in view
+      },
+      once: true // Ensure it only triggers once
+    });
     }
   }, []);
 
